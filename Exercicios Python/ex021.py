c0aed7384ecc67ef1eab091c45236155e5199516
Fedrigo-Dev <fedrigomauro302@gmail.com>
2026-02-23 22:30:46 -0300

    Ex021 Python

--- Exercicios Python/ex021.py
+++ Exercicios Python/ex021.py
@@ -1,1 +1,12 @@
 # Criar um programa que toca um MP3
+
+import pygame
+
+pygame.mixer.init()
+
+pygame.mixer.music.load("lifeletters.mp3")
+
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
